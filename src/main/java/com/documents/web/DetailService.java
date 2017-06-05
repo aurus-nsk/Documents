9d980b29ae70f5d912ad6ef90a37d0fad96a567d
aurus-nsk <aurus7@mail.ru>
2017-06-05 18:09:48 +0700

    work with sprin-boot:run

--- src/main/java/com/documents/web/DetailService.java
+++ src/main/java/com/documents/web/DetailService.java
@@ -1,12 +1,8 @@
 package com.documents.web;
 
-import java.util.concurrent.Future;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
--- src/main/java/com/documents/web/DocumentController.java
+++ src/main/java/com/documents/web/DocumentController.java
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +14,7 @@ import java.util.Map.Entry;
 
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.usermodel.Range;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,10 +27,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class DocumentController {
+	@Value("${appPath}")
+	private String appPath;
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
     public String read() throws Exception {
-		System.out.println("/");
 		return "read_files";
     }
 	
@@ -43,9 +44,9 @@ public class DocumentController {
 	public @ResponseBody String upload(@RequestBody List<String> params) throws Exception {
 		System.out.println("/upload");
 		
-		for(String item : params) {
+		/*for(String item : params) {
 			System.out.println(item);
-		}
+		}*/
 		
 		Map<String, String> map = new LinkedHashMap<String, String>();
 		List<String> fileNames = new ArrayList<String>();
@@ -57,13 +58,9 @@ public class DocumentController {
 			
 			if(values[0].contains("file_secret")){
 				fileNames.add(values[1]);
-				//System.out.println("add " + new String(values[1]));
 			} else {
-				//System.out.println("add values[0]: "+ values[0]);
-				//System.out.println("add values[1]: "+ values[1]);
 				String key = values[0];
 				String value = values[1];
-				
 				map.put(key, value);
 			}
 		}
@@ -71,7 +68,7 @@ public class DocumentController {
 		for(String name : fileNames) {
 			//System.out.println("name " + name);
 			try{
-				FileInputStream in = new FileInputStream(new File("C:\\Users\\Family\\Documents\\Saharov\\patterns\\"+name));
+				FileInputStream in = new FileInputStream(new File(appPath+"\\Saharov\\patterns\\"+name));
 				HWPFDocument doc = new HWPFDocument(in);
 				Range range = doc.getRange(); 
 				//System.out.println("range");
@@ -82,10 +79,9 @@ public class DocumentController {
 					range.replaceText(entry.getKey(), value);
 				}
 				
-				String fileName = "C:\\Users\\Family\\Documents\\Saharov\\documents\\"+new Date().getDate() + "_" + name;
+				String fileName = appPath+"\\Saharov\\documents\\"+new Date().getDate() + "_" + name;
 				System.out.println("output file: " + fileName);
 				doc.write(new FileOutputStream(new File(fileName)));
-				//TODO: move to finally
 				in.close();
 				doc.close();
 			} finally {
@@ -113,7 +109,8 @@ public class DocumentController {
 			int i = name.indexOf('.');
 			String formName = name.substring(0, i);
 			//считываем файл праметром для построения формы
-			String path = "C:\\Users\\Family\\Documents\\Saharov\\form\\" + formName + ".txt";
+			System.out.println("appPath"+appPath);
+			String path = appPath + "\\Saharov\\form\\" + formName + ".txt";
     		//System.out.println(path);
 
 			File f = new File(path);
@@ -142,128 +139,3 @@ public class DocumentController {
         return e.getMessage();  
     }
 }
-/* map.get("")
-for(MultipartFile uploadedFile : uploadingFiles) {
-	HWPFDocument doc = new HWPFDocument(uploadedFile.getInputStream());
-	Range range = doc.getRange();
-	
-	for(Entry<String, String> entry: formParams.entrySet()) {
-		range.replaceText(entry.getKey(), entry.getValue());
-	}
-	
-	Random r = new Random();
-	int num = r.nextInt();
-	//TODO: surname name year
-	String fileName = "C:\\Users\\Family\\Documents\\Сахаров\\Documents\\blank-mesto-zhitelstva" + "" + num +".doc";
-	doc.write(new FileOutputStream(new File(fileName)));
-} */
-/*
-FileInputStream in = new FileInputStream(new File("C:\\Users\\Family\\Documents\\Сахаров\\patterns\\blank-mesto-zhitelstva.doc"));
-HWPFDocument doc = new HWPFDocument(in);
-
-Range range = doc.getRange(); 
-range.replaceText("$фамилия", "Сахаров");
-range.replaceText("$имя", "Дмитрий"); 
-range.replaceText("$отчество", "Иванович"); 
-Random r = new Random();
-int num = r.nextInt();
-String fileName = "C:\\Users\\Family\\Documents\\Сахаров\\patterns\\blank-mesto-zhitelstva" + "" + num +".doc";
-doc.write(new FileOutputStream(new File(fileName))); 
- */
-
-
-//stub
-		/*
-		FileInputStream in = new FileInputStream(new File("C:\\Users\\Family\\Documents\\Сахаров\\patterns\\blank-mesto-zhitelstva.doc"));
-		HWPFDocument doc = new HWPFDocument(in);
-		String regexp = "^{.+}$";
-		//TODO: test от  <фамилия> <имя> <отчество> <р-дд> . <р-мм> . <р-гггг> <ader>
-		//List<String> params = new ArrayList<String>();
-		//for(MultipartFile uploadedFile : uploadingFiles) {
-          //File file = new File(uploadingdir + uploadedFile.getOriginalFilename());
-          //uploadedFile.transferTo(file);
-			//InputStream input = uploadedFile.getInputStream();
-			//HWPFDocument doc = new HWPFDocument(input);
-			//parse reg exp http://stackoverflow.com/questions/12305071/using-apache-poi-with-regex-to-extract-uppercase-words
-			WordExtractor we = new WordExtractor(doc);
-			if (we.getParagraphText() != null) {
-		        String[] dataArray = we.getParagraphText();
-		        for (int i = 0; i < dataArray.length; i++) {
-		            String data = dataArray[i].toString();
-		            System.out.println(data);
-		            Pattern p = Pattern.compile(regexp);
-		            Matcher m = p.matcher(data);
-		            List<String> sequences = new ArrayList<String>();
-		            while (m.find()) {
-		                sequences.add(data.substring(m.start(), m.end()));
-		               // System.out.println(data.substring(m.start(), m.end()));
-		            }
-		            System.out.println("size: " + sequences.size());
-		        }
-		    }
-			
-			//OutputStream output  = new FileOutputStream(new File("C:\\file.doc"));
-			//IOUtils.copy(input, output);
-      //}
-		*/
-
-
-
-
-/*
-@RequestMapping(value="/upload", method = RequestMethod.POST, consumes = { "application/json" })
-	public String upload(@RequestBody() String params) throws Exception {
-		System.out.println("/upload");
-		System.out.println(params);
-		
-		String json = new String(params.substring(1, params.length()-1));
-		String arr[] = json.split(",");
-		
-		List<String> list = new ArrayList<String>();
-		list.addAll(Arrays.asList(arr));
-		
-		HashMap<String, String> map = new HashMap<String, String>();
-		List<String> fileNames = new ArrayList<String>();
-		
-		for(String item:list) {
-			String line = new String(item.substring(1, item.length()-1));
-			String values[] = line.split(":");
-			
-			if(values[0].contains("file_secret")){
-				fileNames.add(values[1]);
-				//System.out.println("add " + new String(values[1]));
-			} else {
-				System.out.println(values[0]);
-				map.put(new String(values[0].getBytes(), "UTF8"), values[1]);
-			}
-		}
-		
-		for(String name : fileNames) {
-			//System.out.println("name " + name);
-
-			try{
-				FileInputStream in = new FileInputStream(new File("C:\\Users\\Family\\Documents\\Saharov\\patterns\\"+name));
-				HWPFDocument doc = new HWPFDocument(in);
-				Range range = doc.getRange(); 
-				//System.out.println("range");
-
-				for(Entry<String, String> entry: map.entrySet()) {
-					System.out.println("replace in doc key: " + entry.getKey() + ", with value: " + entry.getValue());
-					range.replaceText(entry.getKey(), entry.getValue());
-				}
-				
-				String fileName = "C:\\Users\\Family\\Documents\\Saharov\\documents\\"+new Date().getDate() + "_" + name;
-				System.out.println("output file: " + fileName);
-				doc.write(new FileOutputStream(new File(fileName)));
-				//TODO: move to finally
-				in.close();
-				doc.close();
-			} finally {
-				//System.out.println("finally");
-			} 
-		}
-		
-		System.out.println("return");
-		return "index";
-	}
-*/
